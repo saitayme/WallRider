f42704e30dfe3109a1c2ddc1f9226a1ef5f64273
saitayme <115802992+saitayme@users.noreply.github.com>
2024-04-17 12:34:22 +0200

    Delete WallRider directory

--- WallRider/Intermediate/Build/Mac/arm64/WallRiderEditor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "/Users/saitayme/Documents/GitHub/WallRider/WallRider/Intermediate/Build/Mac/arm64/WallRiderEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- WallRider/Intermediate/Build/Mac/arm64/WallRiderEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "/Users/saitayme/Documents/GitHub/WallRider/WallRider/Intermediate/Build/Mac/arm64/WallRiderEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
