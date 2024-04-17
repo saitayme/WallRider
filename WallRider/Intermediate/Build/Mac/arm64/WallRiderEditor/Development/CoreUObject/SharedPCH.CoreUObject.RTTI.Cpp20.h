3f37dba257ec4024757feb0ea18b49fa254802e4
saitayme <115802992+saitayme@users.noreply.github.com>
2024-04-17 12:07:07 +0200

    Unreal Shit

---
+++ WallRider/Intermediate/Build/Mac/arm64/WallRiderEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
+#include "/Users/saitayme/Documents/GitHub/WallRider/WallRider/Intermediate/Build/Mac/arm64/WallRiderEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
+#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
---
+++ WallRider/Intermediate/Build/Mac/arm64/WallRiderEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "/Users/saitayme/Documents/GitHub/WallRider/WallRider/Intermediate/Build/Mac/arm64/WallRiderEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
