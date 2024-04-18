d9c5d5c05c8b82f3d98e02af674491511de2afca
saitayme <115802992+saitayme@users.noreply.github.com>
2024-04-18 17:32:03 +0200

    okiii

---
+++ WallRider/Source/WallRider/Public/Quadrant.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class WALLRIDER_API Quadrants
+{
+public:
+	Quadrant();
+	~Quadrant();
+};
