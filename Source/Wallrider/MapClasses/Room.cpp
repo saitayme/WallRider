2cadd2d921a86e53e7c49a1c1b00255af2dc411c
saitayme <115802992+saitayme@users.noreply.github.com>
2024-05-22 13:51:06 +0200

    done

---
+++ Source/Wallrider/MapClasses/Room.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Room.h"
+
+void URoom::Use(AEntity* Entity, const FString Instructions)
+{
+}
+
+void URoom::Sabotage()
+{
+}
+
+void URoom::MassSabotage()
+{
+}
+
+void URoom::AddEntity(AEntity* Entity)
+{
+	Entities.Add(Entity->EntityId, Entity);
+}
+
+void URoom::RemoveEntity(FString EntityId)
+{
+	if (Entities.Contains(EntityId))
+	{
+		Entities.Remove(EntityId);
+	}
+}
+
+void URoom::Interacted(UObject* Other)
+{
+}
+
+void URoom::Investigated(UObject* Other)
+{
+}
+
+void URoom::ActivateAbilityManually()
+{
+	ActivateAbility();
+}
+
+ATile* URoom::GetRandomTile()
+{
+    int32 RandomIndex = FMath::RandRange(0, Tiles.Num() - 1);
+    return Tiles[RandomIndex]; // Assuming Tiles is an array of tile pointers
+}
+
+TArray<ATile*> URoom::GetAllTiles()
+{
+    return Tiles; // Assuming Tiles is an array of tile pointers
+}
---
+++ Source/Wallrider/MapClasses/Tile.cpp
@@ -0,0 +1,88 @@
+﻿// In case Unreal deletes pointers we still want to use
+
+#include "Tile.h"
+
+#include "MapEnums.h"
+#include "WallRider/Entity/AEntity.h"
+#include "WallRider/Entity/FactionType.h"
+
+
+// Sets default values
+ATile::ATile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ATile::SetEdgeLocked(EEdgeType Edge, bool Locked)
+{
+	Borders[Edge] = Locked ? EBorderType::Locked : EBorderType::None; // use BorderType::None ??? idk man
+}
+
+bool ATile::HasShadewalker()
+{
+	for (const AEntity* entity : CurrentEntities) {
+		if (entity->Faction == EFactionType::ShadeWalker) { 
+			return true;
+		}
+	}
+	return false;
+}
+
+TArray<AEntity*> ATile::GetCurrentEntities()
+{
+	return CurrentEntities;
+}
+
+TMap<EEdgeType, EBorderType> ATile::GetBorders()
+{
+	const TMap<EEdgeType, EBorderType> Copy = Borders;
+	return Copy;
+}
+
+EBorderType ATile::GetBorder(const EEdgeType Direction)
+{
+	const EBorderType Copy = Borders[Direction];
+	return Copy;
+}
+
+void ATile::ChangeCrumValue(float delta)
+{
+	CrumValue += delta;
+}
+
+void ATile::SetRoom(URoom* Room)
+{
+}
+
+float ATile::GetCrumValue()
+{
+	return CrumValue;
+}
+
+// Called when the game starts or when spawned
+void ATile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATile::AddEntity(AEntity* Entity)
+{
+	CurrentEntities.Add(Entity);
+}
+
+void ATile::RemoveEntity(AEntity* Entity)
+{
+	CurrentEntities.Remove(Entity);
+}
+
+ATile* ATile::Clone()
+{
+	return nullptr;
+}
+
+void ATile::SetOnFire(bool onFire)
+{
+    this->IsOnFire = onFire; 
+}
