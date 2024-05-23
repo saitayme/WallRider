dae2b30d52ea3eba5e5ce1e02ca6d9c87b0169fe
saitayme <115802992+saitayme@users.noreply.github.com>
2024-05-23 11:55:20 +0200

    okay

---
+++ WallRider/Intermediate/Build/Mac/arm64/WallRider/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Exceptions.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
+#include "/Users/saitayme/Documents/GitHub/WallRider/WallRider/WallRider/Intermediate/Build/Mac/arm64/WallRider/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Exceptions.Cpp20.h"
+#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
---
+++ WallRider/Intermediate/Build/Mac/arm64/WallRider/Development/Engine/SharedPCH.Engine.Cpp20.InclOrderUnreal5_0.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "/Users/saitayme/Documents/GitHub/WallRider/WallRider/WallRider/Intermediate/Build/Mac/arm64/WallRider/Development/Engine/SharedDefinitions.Engine.Cpp20.InclOrderUnreal5_0.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
